b3d2536f9f494a81d73aee892c9cb134de8516c0
NateShabidoo <98194127+NateShabidoo@users.noreply.github.com>
2023-03-16 21:13:12 -0400

    Delete Web Based Fitness Tracker w Database directory

--- Web Based Fitness Tracker w Database/DBConfig.php
+++
@@ -1,7 +0,0 @@
-//This is the config file used to connect user to the database on host site (local host here)
-<?php
-/*For My LocalPC*/
-// this is using Xampp to run the database, a similar file also works on www.justnormalworkingstudents.com for software engineering team project
-$con = mysqli_connect("localhost","root","") OR die("cannot connect3");
-mysqli_select_db($con, 'fitness_tracker');
-?>
